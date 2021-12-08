23f7391f14d58547ff495fe2f4d174273cd80799
lucatrazzi <luca.trazzi@garaio.com>
2021-12-08 15:41:40 +0100

    day 1 puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod day1;
 fn main() {
     println!("Running AOC 2021");
     println!("Result of puzzle 1 is: {}", day1::puz1().to_string());
+    println!("Result of puzzle 2 is: {}", day1::puz2().to_string());
 }
