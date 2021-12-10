ec1d0d153f5f56196c766080753dfba3cc75a966
lucatrazzi <luca.trazzi@garaio.com>
2021-12-10 12:34:31 +0100

    day2 puzzle2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -7,49 +7,71 @@ pub fn puz1() -> i32 {
 
 pub fn puz2() -> i32 {
     let coordinates = lines_from_file("src/day2/input");
-    todo(coordinates)
+    navigate_ship_after_reading_manual(coordinates)
 }
 
 fn navigate_ship(coordinates: Vec<String>) -> i32 {
-    let forward_coordinates = coordinates.iter().filter(|c| c.starts_with("forward"));
-    let up_coordinates = coordinates.iter().filter(|c| c.starts_with("up"));
-    let down_coordinates = coordinates.iter().filter(|c| c.starts_with("down"));
+    let forward_coordinates = filter_coordinates_by_prefix(&coordinates, "forward");
+    let up_coordinates = filter_coordinates_by_prefix(&coordinates, "up");
+    let down_coordinates = filter_coordinates_by_prefix(&coordinates, "down");
 
-    let horizontal: i32 = forward_coordinates
-        .map(|c| {
-            c.split_whitespace()
-                .nth(1)
-                .unwrap_or("0")
-                .parse::<i32>()
-                .unwrap_or(0)
-        })
-        .sum();
+    let horizontal = extract_units_and_sum(&forward_coordinates);
+    let up = extract_units_and_sum(&up_coordinates);
+    let down = extract_units_and_sum(&down_coordinates);
 
-    let up: i32 = up_coordinates
-        .map(|c| {
-            c.split_whitespace()
-                .nth(1)
-                .unwrap_or("0")
-                .parse::<i32>()
-                .unwrap_or(0)
-        })
-        .sum();
+    let position = horizontal;
+    let depth = down - up;
 
-    let down: i32 = down_coordinates
-        .map(|c| {
-            c.split_whitespace()
-                .nth(1)
-                .unwrap_or("0")
-                .parse::<i32>()
-                .unwrap_or(0)
-        })
-        .sum();
-
-    horizontal * (down - up)
+    position * depth
 }
 
-fn todo(coordinates: Vec<String>) -> i32 {
-    0
+fn navigate_ship_after_reading_manual(coordinates: Vec<String>) -> i32 {
+    let mut position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for coordinate in coordinates {
+        let unit = coordinate
+            .split_whitespace()
+            .nth(1)
+            .unwrap_or("0")
+            .parse::<i32>()
+            .unwrap_or(0);
+
+        if coordinate.starts_with("up") {
+            aim -= unit;
+        } else if coordinate.starts_with("down") {
+            aim += unit;
+        } else if coordinate.starts_with("forward") {
+            position += unit;
+            depth += aim * unit;
+        } else {
+            panic!("Unknown command: {}", coordinate);
+        }
+    }
+
+    position * depth
+}
+
+// TODO: ask explanations about lifetimes
+fn filter_coordinates_by_prefix<'a>(coordinates: &'a Vec<String>, prefix: &str) -> Vec<&'a String> {
+    coordinates
+        .iter()
+        .filter(|c| c.starts_with(prefix))
+        .collect::<Vec<&String>>()
+}
+
+fn extract_units_and_sum(coordinates: &Vec<&String>) -> i32 {
+    coordinates
+        .iter()
+        .map(|c| {
+            c.split_whitespace()
+                .nth(1)
+                .unwrap_or("0")
+                .parse::<i32>()
+                .unwrap_or(0)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -77,16 +99,16 @@ mod tests {
 
     #[test]
     fn puzzle_2_example() {
-        assert_eq!(todo(build_test_input()), 5);
+        assert_eq!(navigate_ship_after_reading_manual(build_test_input()), 900);
     }
 
     #[test]
     fn puzzle_1_answer() {
-        assert_eq!(puz1(), 1557);
+        assert_eq!(puz1(), 1694130);
     }
 
     #[test]
     fn puzzle_2_answer() {
-        assert_eq!(puz2(), 1608);
+        assert_eq!(puz2(), 1698850445);
     }
 }
