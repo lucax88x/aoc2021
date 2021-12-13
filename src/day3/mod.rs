1dc96a4d973e2f296f70b0f59dbacb73236d0590
lucatrazzi <luca.trazzi@garaio.com>
2021-12-13 12:38:25 +0100

    day3 puzzle2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -7,13 +7,13 @@ pub fn puz1() -> i32 {
 
 pub fn puz2() -> i32 {
     let input = util::lines_from_file("src/day3/input");
-    todo(&input)
+    calculate_life_support_rating(&input)
 }
 
 fn calculate_power_consumption(list_of_bits: &Vec<String>) -> i32 {
     let mut gamma: String = String::new();
     let mut epsilon: String = String::new();
- 
+
     let bits_length = list_of_bits[0].len();
 
     for index in 0..bits_length {
@@ -39,13 +39,18 @@ fn calculate_power_consumption(list_of_bits: &Vec<String>) -> i32 {
     bits_to_decimal(&gamma) * bits_to_decimal(&epsilon)
 }
 
-fn todo(coordinates: &Vec<String>) -> i32 {
-    0
+fn calculate_life_support_rating(list_bits: &Vec<String>) -> i32 {
+    let list_bits_slice: Vec<&str> = list_bits.iter().map(|bits| &**bits).collect();
+
+    let oxygen_generator_rating = recursive(true, &list_bits_slice, 0);
+    let co2_scrubber_rating = recursive(false, &list_bits_slice, 0);
+
+    oxygen_generator_rating * co2_scrubber_rating
 }
 
 fn bits_to_decimal(bits: &str) -> i32 {
     let mut decimal = 0;
-    
+
     for (index, bit) in bits.chars().enumerate() {
         let power = bits.len() - index - 1;
         if bit == '1' {
@@ -53,6 +58,38 @@ fn bits_to_decimal(bits: &str) -> i32 {
         }
     }
     decimal
+}
+
+fn recursive(get_bigger: bool, list_bits: &Vec<&str>, index: usize) -> i32 {
+    if list_bits.len() == 1 {
+        return bits_to_decimal(list_bits[0]);
+    }
+
+    let zero_list: Vec<&str> = list_bits
+        .iter()
+        .filter(|bits| bits.chars().nth(index).unwrap() == '0')
+        .map(|bits| *bits)
+        .collect();
+
+    let one_list: Vec<&str> = list_bits
+        .iter()
+        .filter(|bits| bits.chars().nth(index).unwrap() == '1')
+        .map(|bits| *bits)
+        .collect();
+
+    if zero_list.len() > one_list.len() {
+        recursive(
+            get_bigger,
+            if get_bigger { &zero_list } else { &one_list },
+            index + 1,
+        )
+    } else {
+        recursive(
+            get_bigger,
+            if get_bigger { &one_list } else { &zero_list },
+            index + 1,
+        )
+    }
 }
 
 #[cfg(test)]
@@ -76,16 +113,16 @@ mod tests {
 
     #[test]
     fn puzzle_2_example() {
-        assert_eq!(todo(&build_test_input()), 900);
+        assert_eq!(calculate_life_support_rating(&build_test_input()), 230);
     }
 
     #[test]
     fn puzzle_1_answer() {
-        assert_eq!(puz1(), 1694130);
+        assert_eq!(puz1(), 2583164);
     }
 
     #[test]
     fn puzzle_2_answer() {
-        assert_eq!(puz2(), 1698850445);
+        assert_eq!(puz2(), 2784375);
     }
 }
